76ef543196045d1fa72b03c4dca52b85c8057512
johnnybib <jamesgrahamhu@gmail.com>
2017-09-23 23:45:01 -0400

    Basic scheduling funcitonality

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 'use strict';
 
-var SlackBot = require('slackbots');
-
-// create a bot
-var bot = new SlackBot({
-    token: process.env.SLACK_TOKEN, // Add a bot https://my.slack.com/services/new/bot and put the token
-    name: 'makibot'
-});
-
-bot.on('start', function() {
-  console.log("TEST");
-});
+console.log("starting...");
